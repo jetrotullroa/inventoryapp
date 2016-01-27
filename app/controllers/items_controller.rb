964c41711bf7784ea84dac3d9ce4a4adef90f467
jetrotullroa <jetrotullroa@gmail.com>
2016-01-27 16:14:56 +0800

    setup item controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category, :amount)
+  end
+
 end
